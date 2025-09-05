e170b8a5390c0ffb2dd3b009092633f238d574df
Fagner JB <para@fagnerjb.com>
2025-09-05 20:42:00 -0300

    🐛

--- cavwp/wp-content/themes/ctrl/pages/home/components/gallery.php
+++ cavwp/wp-content/themes/ctrl/pages/home/components/gallery.php
@@ -18,20 +18,27 @@ $post_type_object = get_post_type_object($post_type);
 ?>
 <div class="relative snap-start overflow-hidden h-screen">
    <section id="<?php echo $post_type; ?>">
-      <h2 class="sr-only">
+      <h2 class="sr-only absolute z-5 top-6 left-6 text-base uppercase">
          <?php echo $post_type_object->label; ?>
       </h2>
       <div id="<?php echo $post_type; ?>-list"
            class="absolute top-0 left-0 flex items-end h-screen w-min transition-[left]">
          <?php foreach ($all_posts as $item) { ?>
          <?php $Post = new Post($item); ?>
+         <?php $Bg   = new Post($Post->get_meta('background'));
+            ?>
          <article id="<?php echo $Post->get('slug'); ?>"
                   class="relative w-screen h-screen">
             <div class="relative z-5 flex flex-col gap-8 py-12 px-6">
-               <h3 class="text-3xl font-semibold uppercase">
-                  <?php echo $Post->get('title'); ?>
-               </h3>
-               <div class="text-base font-medium">
+               <hgroup class="flex flex-col gap-1">
+                  <h3 class="text-3xl font-semibold uppercase">
+                     <?php echo $Post->get('title'); ?>
+                  </h3>
+                  <p class="text-2xl font-medium">
+                     <?php echo $Post->get('author:name'); ?>
+                  </p>
+               </hgroup>
+               <div class="text-base font-medium max-w-lg">
                   <?php echo $Post->get('summary'); ?>
                </div>
                <?php if (have_rows('links', $item->ID)) { ?>
@@ -39,8 +46,9 @@ $post_type_object = get_post_type_object($post_type);
                   <?php while (have_rows('links', $item->ID)) {
                      the_row(); ?>
                   <li class="flex flex-col gap-1">
-                     <span
-                           class="uppercase text-base"><?php echo get_sub_field('title'); ?></span>
+                     <span class="uppercase text-base">
+                        <?php echo get_sub_field('title'); ?>
+                     </span>
                      <?php if (have_rows('group', $item->ID)) { ?>
                      <ul class="flex rounded border border-neutral-100 divide-x">
                         <?php while (have_rows('group', $item->ID)) {
@@ -61,9 +69,12 @@ $post_type_object = get_post_type_object($post_type);
                   <?php } ?>
                </ul>
                <?php } ?>
+               <div class="text-sm">Foto:
+                  <?php echo $Bg->get('summary', apply_filter: false); ?>
+               </div>
             </div>
             <div class="absolute inset-0 z-1 w-screen h-screen bg-neutral-800/65"></div>
-            <?php echo $Post->get('background', size: 'full', with_html: true, attrs: [
+            <?php echo $Bg->get('thumb', size: 'full', with_html: true, attrs: [
                'class' => 'absolute inset-0 z-0 w-screen h-screen object-cover object-center',
             ]); ?>
          </article>
@@ -73,7 +84,7 @@ $post_type_object = get_post_type_object($post_type);
          <?php foreach ($all_posts as $key => $item) { ?>
          <?php $Post = new Post($item); ?>
          <li
-             class="h-11/12 w-auto <?php 'print' === $post_type ? 'aspect-poster' : 'aspect-video'; ?>">
+             class="h-11/12 w-auto <?php echo 'print' === $post_type ? 'aspect-poster' : 'aspect-video'; ?>">
             <button class="cursor-pointer"
                     title="<?php echo $Post->get('title'); ?>"
                     type="button"
