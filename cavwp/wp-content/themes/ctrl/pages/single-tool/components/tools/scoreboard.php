aeab0ad9dc304dac30c21a9db7d20159b32bc97e
Fagner JB <para@fagnerjb.com>
2026-01-25 20:42:39 -0300

    🐛

--- cavwp/wp-content/themes/ctrl/pages/single-tool/components/tools/scoreboard.php
+++ cavwp/wp-content/themes/ctrl/pages/single-tool/components/tools/scoreboard.php
@@ -80,6 +80,10 @@
             },
 
             removePlayer(index) {
+               if(!confirm('Apagar jogador?')){
+                  return
+               }
+
                this.players.splice(index, 1)
             },
 
@@ -101,41 +105,41 @@
    })
 </script>
 <div x-data="toolScoreboard">
-   <ul class="flex flex-col gap-3 mb-8 text-lg">
+   <ul class="flex flex-col gap-3 mb-8 sm:text-lg">
       <template x-for="({name,color,points},index) in players">
-         <li class="flex items-center gap-1.5">
+         <li class="flex items-center gap-1">
             <button class="btn-alt" type="button" x-on:click.prevent="removePlayer(index)">
                <i class="ri-delete-bin-2-line"></i>
             </button>
-            <span class="size-10 rounded-sm" x-bind:style="`background: ${color}`"></span>
-            <span class="grow text-xl" x-text="name"></span>
-            <button class="btn-alt" type="button" x-on:click.prevent="editPlayer(index,-5)">
+            <span class="shrink-0 size-8 rounded-sm" x-bind:style="`background: ${color}`"></span>
+            <span class="shrink-0 grow sm:text-xl truncate" x-text="name"></span>
+            <button class="btn-alt hidden sm:inline" type="button" x-on:click.prevent="editPlayer(index,-5)">
                -5
             </button>
             <button class="btn-alt" type="button" x-on:click.prevent="editPlayer(index,-1)">
-               -1
+               -<span class="hidden sm:inline">1</span>
             </button>
-            <input class="py-1.5 px-3 w-20 text-center font-bold text-2xl rounded border dark:border-neutral-100" type="number" step="1" x-bind:value="points" x-on:input.prevent="editPlayer(index,0,$el.value)" />
+            <input class="py-1.5 px-3 w-16 sm:w-20 text-center font-bold sm:text-2xl rounded border dark:border-neutral-100" type="number" step="1" x-bind:value="points" x-on:input.prevent="editPlayer(index,0,$el.value)" />
             <button class="btn-alt" type="button" x-on:click.prevent="editPlayer(index)">
-               +1
+               +<span class="hidden sm:inline">1</span>
             </button>
-            <button class="btn-alt" type="button" x-on:click.prevent="editPlayer(index,10)">
+            <button class="btn-alt hidden sm:inline" type="button" x-on:click.prevent="editPlayer(index,10)">
                +10
             </button>
          </li>
       </template>
    </ul>
-   <form class="flex gap-2" x-on:submit.prevent="addPlayer">
-      <select name="color" class="py-1.5 px-3 rounded" x-model="nextColor" x-bind:style="`background: ${colors[nextColor].hex}`" x-bind:class="{'text-neutral-900': colors[nextColor].dark ?? 0}">
+   <form class="flex flex-wrap gap-2" x-on:submit.prevent="addPlayer">
+      <input name="name" class="grow py-1.5 px-3 sm:text-xl rounded border dark:border-neutral-100" placeholder="Novo jogador" type="text" maxlength="16" required />
+       <input name="points" class="py-1.5 px-3 w-14 text-center font-bold sm:text-xl rounded border dark:border-neutral-100" type="number" step="1" value="0" />
+      <select name="color" class="rounded" x-model="nextColor" x-bind:style="`background: ${colors[nextColor].hex}`" x-bind:class="{'text-neutral-900': colors[nextColor].dark ?? 0}">
          <template x-for="({name,hex, dark},index) in colors">
-            <option class="size-10" x-bind:value="index" x-text="name" x-bind:class="dark ?? 0 ? '!text-neutral-900' : '!text-neutral-100'" x-bind:style="`background: ${hex}`"></option>
+            <option x-bind:value="index" x-text="name" x-bind:class="dark ?? 0 ? '!text-neutral-900' : '!text-neutral-100'" x-bind:style="`background: ${hex}`"></option>
          </template>
       </select>
-      <input name="name" class="grow py-1.5 px-3 text-xl rounded border dark:border-neutral-100" type="text" maxlength="20" required />
-      <input name="points" class="py-1.5 px-3 w-20 text-center font-bold text-xl rounded border dark:border-neutral-100" type="number" step="1" value="0" />
       <button class="btn" type="submit">
          <i class="ri-add-circle-line"></i>
-         Adicionar
+         <span class="idden sm:inline">Adicionar</span>
       </button>
    </form>
 </div>
